4c632bd6403a4f7bdc6034088cd0a7158aa63add
yousef69mr <youseffouad69@gmail.com>
2023-06-28 09:26:09 +0200

    7.2.1.1 version

--- TouriscoBackend/settings.py
+++ TouriscoBackend/settings.py
@@ -238,7 +238,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-MEDIAFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# MEDIAFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
